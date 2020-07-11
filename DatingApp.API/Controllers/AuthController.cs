f0bc98c91c2bdd46cdf157096aa88f50b40beebb
kassarin saraporn <mo.jase.soulmate@gmail.com>
2020-07-11 16:48:07 +0700

    update 2020-07-11

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -46,39 +46,49 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            var userFromRepo = await _repo.Login(userForLoginDto.Username
-                .ToLower(), userForLoginDto.Password);
 
-            if (userFromRepo == null)
-                return Unauthorized();
+            try{
+                //throw new Exception("Computer says no!");
 
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
-                new Claim(ClaimTypes.Name, userFromRepo.Username)
-            };
+                var userFromRepo = await _repo.Login(userForLoginDto.Username
+                    .ToLower(), userForLoginDto.Password);
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+                if (userFromRepo == null)
+                    return Unauthorized();
 
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                    new Claim(ClaimTypes.Name, userFromRepo.Username)
+                };
 
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(1),
-                SigningCredentials = creds
-            };
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
 
-            var tokenHandler = new JwtSecurityTokenHandler();
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-            var token = tokenHandler.CreateToken(tokenDescriptor);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(claims),
+                    Expires = DateTime.Now.AddDays(1),
+                    SigningCredentials = creds
+                };
 
-            //var user = _mapper.Map<UserForListDto>(userFromRepo);
+                var tokenHandler = new JwtSecurityTokenHandler();
 
-            return Ok(new
-            {
-                token = tokenHandler.WriteToken(token)
-            });
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                //var user = _mapper.Map<UserForListDto>(userFromRepo);
+
+                return Ok(new
+                {
+                    token = tokenHandler.WriteToken(token)
+                });
+            
+            }
+            catch{
+                return StatusCode(500,"Computer really says no!");
+            }
+            
         }
     }
 
